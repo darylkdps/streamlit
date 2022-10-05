96e96529740f97b1dabd528a7d989f3ab525f4b0
darylkdps <darylkdps-github@yahoo.com>
2022-10-05 11:08:02 +0800

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 def main():
     st.title('Home Page - Test of Streamlit Functionality')
+    st.text(st.secrets['test_token'])
 
 if __name__ == '__main__':
     st.set_page_config(page_title="Daryl Test", page_icon="random", layout="wide", initial_sidebar_state="auto")
