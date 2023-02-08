077a21e72ad2ded83442987618640ae5843ac8e7
darylkdps <darylkdps-github@yahoo.com>
2023-02-08 11:57:28 +0800

    revert back to previous state

--- app/Home.py
+++ app/Home.py
@@ -1,69 +1,15 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import spacy
+from pathlib import Path
 
-st.set_page_config(
-    page_title='Semantic Ruler',
-    page_icon='random',
-    layout='wide',
-    initial_sidebar_state='auto'
-    )
+def main():
+    st.title('Home Page - Test of Streamlit Functionality')
 
-st.title('Semantic Ruler')
+    if 'D:' in str(Path.cwd()) or 'C:' in str(Path.cwd()):
+        st.markdown('_No token_')
+    else:
+        st.markdown('_' + st.secrets['test_token'] + '_')
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    spacy.require_cpu()
-    return spacy.load('en_core_web_lg')
-nlp = load_model()
+if __name__ == '__main__':
+    st.set_page_config(page_title="Daryl Test", page_icon="random", layout="wide", initial_sidebar_state="auto")
+    main()
 
-default_title_word = 'leadership'
-default_content_words = '''paper, pen, priority, decision, effort, mentor, stewardship, education, accountability, governance, leader, visualisation, cuisine, transform, chemistry, translate'''
-
-title_word = st.text_input(
-    label='Input Title Word:',
-    value=default_title_word,
-    )
-
-content_words = st.text_input(
-    label='Input content words separated by a comma:',
-    value=default_content_words,
-    )
-
-title_word_cleaned = list(map(str.strip, title_word))
-content_words_cleaned = list(map(str.strip, content_words.split(',')))
-
-df = pd.DataFrame(data={'Title Word': [title_word] * len(content_words_cleaned), 'Content Word': content_words_cleaned})
-df['Cosine Similarity'] = df.apply(lambda row: nlp.vocab[row['Title Word']].similarity(nlp.vocab[row['Content Word']]), axis=1).astype(float).round(3)
-df = df.sort_values(by=['Cosine Similarity'])
-# st.dataframe(df)
-
-fig = px.bar(
-    df,
-    x='Cosine Similarity',
-    y='Content Word',
-    range_x=[0,1],
-    text_auto='.3f',
-    text='Cosine Similarity',
-    template='plotly_dark',
-    title='Cosine Similarity between Title Word <b>%s</b> and <i>content word</i>' % title_word.upper(),
-    width=1000,
-    height=1000
-)
-
-fig.update_traces(
-    # marker_color='cyan'
-)
-
-fig.update_layout(
-    title_x=0.5,
-    title_font_size=18,
-    xaxis_dtick=0.1,
-    )
-
-st.plotly_chart(
-    fig,
-    use_container_width=True,
-    theme=None,
-    )
